68af385d8bc11354eb313dc13d223b1999c5f094
watermelon2718 <ellyntran@my.cwi.edu>
2025-04-22 23:06:50 -0600

    Update darkmode.js

--- private-browsing-theme/darkmode.js
+++ private-browsing-theme/darkmode.js
@@ -1,16 +1,23 @@
+document.body.style.backgroundColor = 'black';
+
+document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, a, ul, div, tr, .container').forEach(function(element) {
+    element.style.color = 'white';
+    element.style.backgroundColor = 'black';
+    link.style.color = 'cyan';
+});
+
 // document.querySelectorAll('p').forEach(function(p) {
 //     p.style.color = 'white';
 // });
 
-document.querySelectorAll('p').forEach(function(p) {
-    p.style.backgroundColor = 'black';
-});
+// document.querySelectorAll('p').forEach(function(p) {
+//     p.style.backgroundColor = 'black';
+// });
 
-document.body.style.backgroundColor = 'black';
 
-document.querySelectorAll('.container').forEach(function(container) {
-    container.style.backgroundColor = 'black';
-});
+// document.querySelectorAll('.container').forEach(function(container) {
+//     container.style.backgroundColor = 'black';
+// });
 
 // document.querySelectorAll('a').forEach(function(header) {
 //     primary.style.color = 'white';
@@ -21,31 +28,31 @@ document.querySelectorAll('.container').forEach(function(container) {
 //     header.style.color = 'white';
 // });
 
-document.querySelectorAll('a').forEach(function(link) {
-    link.style.color = 'cyan';
-});
+// document.querySelectorAll('a').forEach(function(link) {
+//     link.style.color = 'cyan';
+// });
 
-document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, a').forEach(function(element) {
-    element.style.color = 'white';
-});
+// document.querySelectorAll('p, h1, h2, h3, h4, h5, h6, span, a').forEach(function(element) {
+//     element.style.color = 'white';
+// });
 
-document.querySelectorAll('span').forEach(function(span) {
-    span.style.backgroundColor = 'black';
-});
+// document.querySelectorAll('span').forEach(function(span) {
+//     span.style.backgroundColor = 'black';
+// });
 
-document.querySelectorAll('ul').forEach(function(ul) {
-    ul.style.backgroundColor = 'black';
-    ul.style.color = 'white';
-});
+// document.querySelectorAll('ul').forEach(function(ul) {
+//     ul.style.backgroundColor = 'black';
+//     ul.style.color = 'white';
+// });
 
-document.querySelectorAll('div').forEach(function(div) {
-    div.style.backgroundColor = 'black';
-    div.style.color = 'white';
-});
+// document.querySelectorAll('div').forEach(function(div) {
+//     div.style.backgroundColor = 'black';
+//     div.style.color = 'white';
+// });
 
-document.querySelectorAll('tr').forEach(function(tr) {
-    tr.style.backgroundColor = 'black';
-    tr.style.color = 'white';
-});
+// document.querySelectorAll('tr').forEach(function(tr) {
+//     tr.style.backgroundColor = 'black';
+//     tr.style.color = 'white';
+// });
 
 
